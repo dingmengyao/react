7dafd710800a419be498256147f70baabfb73670
Dingmy <13930877187@163.com>
2019-10-29 17:19:16 +0800

    xxxx

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('this is a react project')
+import React from 'react'
+import ReactDom from 'react-dom'
+
+
+class App extends React.Component{
+    render(){
+        return <h1>这是一个react组件</h1>
+    }
+}
+
+ReactDom.render(
+    <App />,
+    document.querySelector('#root')
+)
